f742b15034e6024a743c5bfc9569f7cd6575e53a
jeffnb <jeffnb@gmail.com>
2021-01-10 14:29:05 -0800

    Adding full flow

--- flows/wallpaperfetcher.py
+++ flows/wallpaperfetcher.py
@@ -1,21 +1,98 @@
-import os
-import time
-from urllib import request
-
-import boto3
-from prefect import task, Flow, Parameter
-import prefect
-import praw
 import datetime
+import logging
+import os
+from urllib import request
 from urllib.parse import urlparse
 
+import boto3
+import praw
+import prefect
+from PIL import Image
+from imgurpython import ImgurClient
+from imgurpython.helpers.error import ImgurClientError
+from prefect import task, Flow, Parameter
 from prefect.executors import LocalDaskExecutor
 from prefect.schedules import IntervalSchedule
+from prefect.storage import GitHub
+from prefect.tasks.control_flow import FilterTask
 from prefect.utilities.edges import unmapped
 from slugify import slugify
-from prefect.tasks.control_flow import FilterTask
-from .imgur_wrapper import ImgurWrapper
-from PIL import Image
+
+
+# Has to be in the same file or needs to be on every machine
+class ImgurWrapper:
+    def __init__(self, client_id, client_secret):
+        self.__client = ImgurClient(client_id, client_secret)
+
+    @staticmethod
+    def is_imgur(url):
+        """
+        Simple check to see if url is an imgur url
+        :param url: ParsedUrl of the
+        :return: boolean if it is a imagr or not
+        """
+        return "imgur.com" in url.hostname
+
+    @staticmethod
+    def is_album(url):
+        if "/a/" not in url.path:
+            return False
+        else:
+            return True
+
+    def get_image_list(self, url):
+        """
+        This call is intended to take the url and return a list of all images associated with it.  It will parse
+        the image without extension or parse all images.
+        :param url: parsed url object
+        :return: list of images
+        """
+        image_list = []
+        if self.is_album(url):
+            image_list = self.get_album_images(url)
+        else:
+            image = self.get_image(url)
+            if image is not None:
+                image_list.append(image)
+
+        return image_list
+
+    def get_image(self, url):
+        """
+        Get a single image from a url
+        :param url: parsed url
+        :return: an image or None if exception raised
+        """
+        image_id = url.path[url.path.rfind("/") + 1 :]
+        try:
+            image = self.__client.get_image(image_id)
+        except ImgurClientError as e:
+            logging.error(
+                "Status Code: " + e.status_code + " Error: " + e.error_message
+            )
+            image = None
+
+        return image
+
+    def get_album_images(self, url):
+        """
+        Gets all the images in an album as a list of image objects
+        :param url: parsed url
+        :return: Either a list of images or an empty list
+        """
+        album_id = url.path[url.path.rfind("/") + 1 :]
+        image_list = []
+
+        try:
+            images = self.__client.get_album_images(album_id)
+        except ImgurClientError as e:
+            logging.error(
+                "Status Code: " + str(e.status_code) + " Error: " + e.error_message
+            )
+        else:
+            image_list = images
+
+        return image_list
 
 
 def get_suffix(imagename):
@@ -153,6 +230,11 @@ with Flow("Reddit wallpaper fetcher", executor=LocalDaskExecutor(), schedule=sch
     # finally add the submission data to the s3 bucket
     final = store_submissions.map(unmapped(bucket), unique)
 
+flow.storage = GitHub(
+    repo="jeffnb/wallpaper-prefect",                 # name of repo
+    path="flows/wallpaperfetcher.py",        # location of flow file in repo
+    secrets=["GITHUB_ACCESS_TOKEN"]  # name of personal access token secret
+)
 
 flow.register("Testing")
 # flow.visualize()
